6ddb9e8d3bfe2509bfa4e2d64c48b0360c6e3280
Kevin <cookblob@gmail.com>
2025-09-15 00:13:07 +0800

    implement function record_tomato

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import datetime
 import time
 import math
 import os
+import json
+
+tomato_records_path = "/home/Qtmd/pylab/simple-scheduler/data/tomato-records.json"
+task_pool_path = "/home/Qtmd/Journal/gaokao/task pool.md"
 
 def main():
-    task_pool_path = "/home/Qtmd/Journal/gaokao/task pool.md"
-    tomato_clock_emulation()
+    end_time = tomato_clock_emulation()
+    record_tomato(end_time)
     manipulate_tasks_pool()
     play_end_riff()
 
@@ -24,8 +28,20 @@ def tomato_clock_emulation():
         time.sleep(1)
         time_remain = end_time - datetime.datetime.now()
     print("A tomato is done!")
-    # TODO: write records to json file
+    return end_time
 
+def record_tomato(end_time):
+    today = datetime.datetime.now().strftime("%Y/%m/%d") # a string representing today's date for record access
+    with open(tomato_records_path, "w+", encoding="utf-8") as f:
+        tomato_records = json.load(f)
+        # read the record for today
+        tomato_record = tomato_records[today]
+        # add value str(end_time) to the list of completion_times
+        tomato_record["completion_times"].append(str(end_time))
+        # count up number of tomatos
+        tomato_record["tomato_number"] += 1
+        # write back to json file
+        json.dump(tomato_records, f, indent=4)
 
 def play_end_riff():
     print("play end riff...")
